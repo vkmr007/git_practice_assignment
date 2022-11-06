ecc8a1414141437175a24b2db3658645507fc7c2
vkmr007 <omkarkumar0712@gmail.com>
2022-11-06 20:50:13 +0530

    added prime code

---
+++ prime.js
@@ -0,0 +1,23 @@
+
+
+//shown prime
+let num=101;
+
+ for (let i=2; i<=num; ++i)
+ {
+   if (i<num)
+   {  
+    //  if (num%i===0)
+    //  console.log("No");
+     
+    // if (num%i!==0)
+    //  continue;
+   }
+    
+else if (num===2 || num===1)
+console.log("Yes");
+     
+else if(num===i)
+console.log("Yes");
+break;
+ }
